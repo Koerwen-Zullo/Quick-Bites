346281225371bd2bd7306f4a45b3203a39dab834
Koerwen-Zullo <koerwenzullo@gmail.com>
2026-05-29 23:22:47 +0800

    added prisma orm

--- client-side/src/App.tsx
+++ client-side/src/App.tsx
@@ -1,71 +1,11 @@
-import { add, format } from "date-fns";
-import { useState } from "react";
-function App() {
-  const [name, setName] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  };
-  const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (name.trim() === "" || password.trim() === "") {
-      window.alert("error empty name or password field");
-      return;
-    }
 
-    const formData = {
-      name: name,
-      password: password,
-    };
-    try {
-      const response = await fetch("http://localhost:5000/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        window.alert(data.message);
-      }
-    } catch (error) {
-      console.error(error);
-    } finally {
-      setName("");
-      setPassword("");
-    }
-  };
+export default function App() {
   return (
-    <>
-      <div>
-        Tomorrow's Date: {format(add(new Date(), { days: 1 }), "do MMMM yyyy")}
-      </div>
-      <div>
-      <form onSubmit={handleSubmit}>
-        <input
-          value={name}
-          placeholder="name"
-          type="text"
-          onChange={handleName}
-        />
-        <br />
-        <input
-          value={password}
-          placeholder="password"
-          type="password"
-          onChange={handlePassword}
-        />
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-      </div>
-    </>
+    <div>
+    <h1>hello</h1>
+    </div>
   );
-}
+};
 
-export default App;
+
+
--- server-side/src/server.js
+++ server-side/src/server.js
@@ -1,27 +1,9 @@
-const express = require('express');
-const cors = require('cors')
-const port = 5000
-const app = express();
+import dotenv from 'dotenv'
+dotenv.config()
+import app from './app.js'
 
-app.use(cors())
-app.use(express.urlencoded({ extended: false}))
-app.use(express.json())
+const port = process.env.PORT;
 
-
-app.post("/", async (req, res) => {
-    const { name, password } = req.body
-
-    if (!name || !password) {
-        return res.status(400).json({
-            message: "error empty name or password field"
-        })
-    }
-    return res.status(200).json({
-        message: `your name is ${name} 
-    and your password is ${password}`
-    })
-})
-
-app.listen(port, () =>{
-    console.log(`server running on port: ${port}`)
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
 })
